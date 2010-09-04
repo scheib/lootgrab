c937e2c1a6a12f93a424d5f05a8a651e00f91e59
Nat Duca <nduca@chromium.org>
2010-09-04 12:07:04 -0700

    Bloop

--- lootgrab/world.js
+++ lootgrab/world.js
@@ -1,20 +1,32 @@
 tdl.provide('world')
 
-function Tile(id,json) {
+function EntityID(world,id,json) {
     self.id = id
+    
+}
+
+function Cell(world,id,json) {
+    self.id = id;
 }
 
 function World(json) {
-    this.width = json.width;
-    this.height = json.height;
-    this.tiles = []
+    this.width = json.world.width;
+    this.height = json.world.height;
+
     this.entities = {}
-    for(var y = 0; y < this._height; ++y) {
-        for(var x = 0; x < this._width; ++x) {
-            var tile = new Tile("tile" + x + "_" + y, x, y)
-            this.tiles.append(tile)
-            this.world[tile.id] = tile
+    for(var entID in json.entities) {
+        if(hasOwnProperty(json.entities,entID)) {
+            var ent = new Entity(this,entID,json.entities[entID])
+            this.entitis[entID] = ent
         }
+    }
+    
+    this.cells = []
+    var i = 0
+    for(var i = 0; i < json.world.cells.length; ++i) {
+        var cellID = json.world.cells[i++]
+        var cell = new Cell(this,cellID, json.cells[cellID])
+        this.cells.push(cell)
     }
 }
 
