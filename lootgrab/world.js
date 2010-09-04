145bf2cda26e24edaa03b46dac4a971bbb0542b3
nduca <nduca@chromium.org>
2010-09-04 11:30:47 -0700

    World scratch

---
+++ lootgrab/world.js
@@ -0,0 +1,16 @@
+tdl.provide('world')
+
+function World(json) {
+    this.width = json.width;
+    this.height = json.height;
+    this.tiles = Array(width * height)
+    this.entities = {}
+    for(var y = 0; y < this._height; ++y) {
+        for(var x = 0; x < this._width; ++x) {
+            var tile = new Tile("tile" + x + "_" + y, x, y)
+            this.tiles.append(tile)
+            this.world[tile.id] = tile
+        }
+    }
+}
+
