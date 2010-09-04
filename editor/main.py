3da96dd2d7ffe58a018386518baed225c4555205
gmurphy <gmurphy@gmail.com>
2010-09-04 15:08:00 -0700

    App engine update

--- editor/main.py
+++ editor/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-
+import os, sys
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
 from google.appengine.ext import db
-from google.appengine.api import images
 from django.utils import simplejson
 
 # Functions
@@ -25,26 +25,30 @@ def id_encode(n):
   return ''.join(reversed(s))
 
 def id_decode(s):
-    if s[0] == SIGN_CHARACTER:
-        return -num_decode(s[1:])
-    n = 0
-    for c in s:
-        n = n * BASE + ALPHABET_REVERSE[c]
-    return n
+  if s[0] == SIGN_CHARACTER:
+    return -num_decode(s[1:])
+  n = 0
+  for c in s:
+    n = n * BASE + ALPHABET_REVERSE[c]
+  return n
 
 # Models
 class Entity(db.Model):
   name = db.StringProperty()
-  img = db.BlobProperty()
   img_url = db.StringProperty()
+  x = db.IntegerProperty(default=0)
+  y = db.IntegerProperty(default=0)
+  width = db.IntegerProperty(default=0)
+  height = db.IntegerProperty(default=0)
 
   def get_id(self):
     return id_encode(self.key().id())
 
   def get_data(self):
     return {
+      'id' : self.get_id(),
       'name' : self.name,
-      'img' : '/images/' + self.get_id()
+      'img' : self.img_url
     }
 
 def dbEntity(id):
@@ -60,6 +64,7 @@ class Cell(db.Model):
   
   def get_data(self):
     return {
+      'id' : self.get_id(),
       'ground' : self.ground,
       'entities' : self.entities
     }
@@ -70,8 +75,8 @@ def dbCell(id):
 
 class World(db.Model):
   name = db.StringProperty()
-  width = db.NumberProperty()
-  height = db.NumberProperty()
+  width = db.IntegerProperty()
+  height = db.IntegerProperty()
   cells = db.ListProperty(str, default=[])
 
   def get_id(self):
@@ -79,11 +84,31 @@ class World(db.Model):
 
   def get_data(self):
     return {
+      'id' : self.get_id(),
       'name' : self.name,
       'width' : self.width,
       'height' : self.height,
       'cell' : self.cells
     }
+
+  def get_all_data(self):
+    data = self.get_data()
+    data['cells'] = {}
+    data['entities'] = {}
+
+    for cell_id in self.cells:
+      if not cell_id:
+        continue
+
+      cell = dbCell(cell_id)
+
+      data.cells[cell.get_id()] = cell.get_data()
+
+      for entity_id in cell.entities:
+        if entity_id not in data.entities:
+          data.entities[entity_id] = cell.get_data()
+    return data
+
 
 def dbWorld(id):
   return World.get_by_id(int(id_decode(id)))
@@ -97,20 +122,19 @@ class MainHandler(webapp.RequestHandler):
 
 class SaveEntity(webapp.RequestHandler):
   def post(self):
-    data = simplejson.parse(self.request.get("data"))
+    data = simplejson.loads(self.request.get("data"))
     img = self.request.get('imgfile')
 
-    if data.id:
+    entity = None
+    if 'id' in data:
       entity = dbEntity(data.id)
     
     if not entity:
       entity = Entity()
     
-    if self.request.get("imgfile"):
-      entity.img = self.request.get("imgfile")
-      entity.mimetype = 'image/png' #hax
-    if self.request.get("name"):
-      entity.name = self.request.get("name")
+    entity.img_url = data.img_url
+    entity.mimetype = 'image/png'
+    entity.name = data.name
 
     entity.put()
 
@@ -128,27 +152,27 @@ class GetImage(webapp.RequestHandler):
 
 class SaveCell(webapp.RequestHandler):
   def post(self):
-    data = simplejson.parse(self.request.get("data"))
+    data = simplejson.loads(self.request.get("data"))
 
-    if data.id:
+    cell = None
+    if 'id' in data:
       cell = dbCell(data.id)
     
     if not cell:
       cell = Cell()
     
     # TODO(glen): Validate data.
-    cell.ground = self.request.get("ground")
-    cell.entities = self.request.get("entites")
+    cell.ground = int(self.request.get("ground"))
+    cell.entities = self.request.get("entities")
     cell.put()
 
     self.response.out.write(cell.get_id())
 
-
 class SaveWorld(webapp.RequestHandler):
   def post(self):
-    data = simplejson.parse(self.request.get("data"))
-
-    if data.id:
+    data = simplejson.loads(self.request.get("data"))
+    world = None
+    if 'id' in data:
       world = dbWorld(data.id)
     
     if not world:
@@ -156,10 +180,12 @@ class SaveWorld(webapp.RequestHandler):
     
     # TODO(glen): Validate data.
     world.name = self.request.get("name")
-    world.width = self.request.get("width")
-    world.height = self.request.get("height")
+    world.width = int(self.request.get("width"))
+    world.height = int(self.request.get("height"))
     world.cells = self.request.get("cells")
     world.put()
+
+    self.response.out.write(world.get_id())
 
 
 class GetWorld(webapp.RequestHandler):
@@ -168,24 +194,33 @@ class GetWorld(webapp.RequestHandler):
     if not world:
       return
     
-    data = {}
-    data.cells = {}
-    data.entities = {}
-
-    for cell_id in world.cells:
-      cell = dbCell(cell_id)
-
-      data.cells[cell.get_id()] = cell.get_data()
-
-      for entity_id in cell.entities:
-        if entity_id not in data.entities:
-          data.entities[entity_id] = cell.get_data()
+    data = world.get_all_data()
     self.response.out.write(simplejson.dumps(data))
+
+class DbgEditor(webapp.RequestHandler):
+  def get(self):
+    worlds = World.all().fetch(1000)
+    worldlist = [];
+    for world in worlds:
+      worldlist.append({
+        'name' : world.name,
+        'id' : world.get_id()
+      })
+
+    world_json = ""
+    if self.request.get("id"):
+      world_json = simplejson.dumps(dbWorld(self.request.get("id")).get_all_data())
+
+    path = os.path.join(os.path.dirname(__file__), "files/dbgeditor.html")
+    self.response.out.write(template.render(path, {
+      'worlds' : worldlist,
+      'world_json' : world_json
+    }))
 
 # Main
 def main():
   application = webapp.WSGIApplication([
-    ('/', MainHandler),
+    ('/', DbgEditor),
     ('/api/saveentity', SaveEntity),
     ('/api/savecell', SaveCell),
     ('/api/saveworld', SaveWorld),
