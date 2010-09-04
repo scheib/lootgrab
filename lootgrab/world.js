b2164129862db90c47b0801481afae71399c8dc4
Nat Duca <nduca@chromium.org>
2010-09-04 16:18:42 -0700

    Cleanup.

--- lootgrab/world.js
+++ lootgrab/world.js
@@ -1,51 +1,6 @@
 tdl.provide('world')
 
-function ImageEntity(world,entID) {
-  var def = world.get_def(entID);
-  var that = this;
-  this.img = new Image();
-  this.img.src = def.img;
-  this.img.onload = function() { 
-    tdl.log(that.img.src + " loaded");
-  };
-  this.img.onerror = function() { 
-    tdl.log(that.img.src + " **FAILED**"); 
-  };
-}
-
-ImageEntity.prototype.draw = function(ctx, x,y,w,h) {
-  ctx.drawImage(this.img, x,y,w,h);
-}
-
-function TileEntity(world,entID) {
-  var tile_def = world.get_def(entID)
-  if(tile_def === undefine)
-    throw "Tile def could not be found!"
-  
-  var tileset_def = world.get_def(tile_def.tileset)
-  if(tilset_def === undefined)
-    throw "Tileset could not be found"
-}
-TileEntity.prototype.draw = function(ctx, x,y,w,h) {
-  ctx.drawImage(this.img, x,y,w,h);
-}
-
 ///////////////////////////////////////////////////////////////////////////
-
-function Cell(world,id,x,y,json) {
-  this.id = id;
-  this.x = x;
-  this.y = y;
-  this.ground_ent = world.newEntity(json.ground_id)
-  if(this.ground_ent === undefined)
-    throw "Could not instantiate " + json.ground_id
-}
-
-Cell.prototype.draw = function(ctx,x,y,w,h) {
-  ctx.strokeStyle = "rgb(255,255,0)";
-  ctx.strokeRect(x,y,w,h);
-  this.ground_ent.draw(ctx,x,y,w,h);
-}
 
 function World(json) {
   this.width = json.world.width;
@@ -67,25 +22,26 @@ function World(json) {
   }
 }
 
-World.prototype.get_def = function(entID) {
-  return this._entity_defs[entID];
-}
-
-World.prototype.newEntity = function(entID) {
+World.prototype.newEntity = function(entDefID) {
   that = this;
-  def = this.get_def(entID);
+  def = this.getDef(entDefID);
   if(def === undefined)
-    throw "No entity def found for " + entID;
+    throw "No entity def found for " + entDefID;
   
   if(def.type == "image") {
-    e = new ImageEntity(this,entID)
+    e = new ImageEntity(this,entDefID)
   } else if(def.type == "tile") {
-    e = new TileEntity(this,entID);
+    e = new TileEntity(this,entDefID);
   } else {
     throw "Un instantiable ent type: " + def.type
   }
-  this.id = entID;
+  this.id = entDefID;
   return e;
+}
+
+
+World.prototype.getDef = function(entDefID) {
+  return this._entity_defs[entDefID];
 }
 
 World.prototype.draw_dbg = function (ctx) {
@@ -93,9 +49,9 @@ World.prototype.draw_dbg = function (ctx) {
   cell_height = ctx.canvas.height / this.height;
   for(var i = 0; i < this.cells.length; ++i) {
     var cell =this.cells[i];
-    cell.draw(ctx,
-            cell_width * cell.x,
-            cell_height * cell.y,
-            cell_width, cell_height);
+    ctx.strokeStyle = "rgb(255,255,0)";
+    ctx.strokeRect(cell_width * cell.x,
+                   cell_height * cell.y,
+                   cell_width, cell_height);
   }
 }
