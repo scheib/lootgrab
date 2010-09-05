def74fc0fe4120d9c4112b1d08c5202481a1908c
Ian Lewis <ilewis@google.com>
2010-09-04 19:00:03 -0700

    debug

---
+++ lootgrab/actor.js
@@ -0,0 +1,51 @@
+tdl.provide("actor")
+
+tdl.require("lootgrab.math")
+
+/**
+ *
+ * @param {World} w
+ * @param entDef
+ */
+function Actor(w, entDef) {
+  this.world = w;
+  this.entityDef = entDef;
+
+  this.position = ('position' in entDef)
+      ? new Vec2(entDef.position.x + .5,  entDef.position.y + .5)
+      : new Vec2(1.5,1.5);
+  this.heading = Vec2.CENTER;
+  this.speed = .1;
+  this.radius = .25;
+
+  this.isAlive = true;
+
+  this.sprite = this.world.newEntity(entDef.render_tile);
+}
+
+
+/**
+ * Update the actor at the beginning of the frame
+ *
+ * @param tick
+ * @param elapsed
+ */
+Actor.prototype.update = function(tick, elapsed) {
+  this.updatePosition();
+}
+
+/**
+ * Inform the actor that it has collided with another actor
+ *
+ * @param {Actor} other
+ */
+Actor.prototype.onCollide = function(other) {
+  
+}
+
+Actor.prototype.updatePosition = function(elapsed) {
+  this.position = this.position.add (
+      this.heading.mul(this.speed));
+
+}
+
