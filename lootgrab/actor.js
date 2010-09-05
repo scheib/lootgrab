da14224df6bfcde2d0a699825921c892d7d5390b
Vincent Scheib <scheib@gmail.com>
2010-09-04 18:10:07 -0700

    screen shot

---
+++ lootgrab/actor.js
@@ -0,0 +1,37 @@
+tdl.provide("actor")
+
+tdl.require("math")
+
+function Actor(entDef) {
+  this.entityDef = entDef;
+
+}
+
+Actor.prototype.position = new Vec2();
+Actor.prototype.heading = Vec2.CENTER;
+Actor.prototype.speed = .1;
+Actor.prototype.radius = .25;
+
+Actor.prototype.isAlive = true;
+
+/**
+ * Update the actor at the beginning of the frame
+ *
+ * @param tick
+ * @param elapsed
+ */
+Actor.prototype.update = function(tick, elapsed) {
+  
+}
+
+/**
+ * Inform the actor that it has collided with another actor
+ *
+ * @param {Actor} other
+ */
+Actor.prototype.onCollide = function(other) {
+  
+}
+
+
+
