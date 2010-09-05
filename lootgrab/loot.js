5213b1910a9edb327ff1b8113755e4da9eb8d751
Adrienne Walker <enne.walker@gmail.com>
2010-09-05 12:16:07 -0700

    Add sound for win condition.

---
+++ lootgrab/loot.js
@@ -0,0 +1,63 @@
+tdl.provide('lootgrab.loot');
+tdl.require('lootgrab.actor');
+
+tdl.require('tdl.log');
+
+function Loot(w, def) {
+  this.loot = true;
+  Actor.call(this, w, def);
+};
+tdl.base.inherit(Loot, Actor);
+
+Loot.prototype.onCollide = function(other) {
+  // get "picked up"
+  if (other == this.world.hero) {
+    this.loot = false;
+    this.kill();
+  }
+};
+
+function Key(w, def) {
+  Loot.call(this, w, def);
+};
+tdl.base.inherit(Key, Loot);
+
+function Shield(w, def) {
+  Loot.call(this, w, def);
+};
+tdl.base.inherit(Shield, Loot);
+
+function Fire(w, def) {
+  Loot.call(this, w, def);
+};
+tdl.base.inherit(Fire, Loot);
+
+function Wand(w, def) {
+  Loot.call(this, w, def);
+};
+tdl.base.inherit(Wand, Loot);
+
+function Cross(w, def) {
+  Loot.call(this, w, def);
+};
+tdl.base.inherit(Cross, Loot);
+
+function Sword(w, def) {
+  Loot.call(this, w, def);
+};
+tdl.base.inherit(Sword, Loot);
+
+function Meat(w, def) {
+  Loot.call(this, w, def);
+};
+tdl.base.inherit(Meat, Loot);
+
+function GoldChest(w, def) {
+  Loot.call(this, w, def);
+};
+tdl.base.inherit(GoldChest, Loot);
+
+function GoldBars(w, def) {
+  Loot.call(this, w, def);
+};
+tdl.base.inherit(GoldChest, Loot);
