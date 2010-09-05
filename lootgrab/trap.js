df28ec43152caf60a34a7300d75a978d3a81c91c
gman <gman@google.com>
2010-09-05 15:23:34 -0700

    loading

---
+++ lootgrab/trap.js
@@ -0,0 +1,41 @@
+tdl.provide('lootgrab.trap');
+tdl.require('lootgrab.actor');
+
+tdl.require('tdl.log');
+
+function Trap(w, def) {
+  Actor.call(this, w, def);
+};
+tdl.base.inherit(Trap, Actor);
+
+function Trapdoor(w, def) {
+  Trap.call(this, w, def);
+  this.open = false;
+  this.openSprite = def.openSprite;
+};
+tdl.base.inherit(Trapdoor, Trap);
+
+var minTrapdoorDistance = 0.1;
+
+
+Trapdoor.prototype.onCollide = function(other) {
+  if (other.position.sub(this.position).len() < minTrapdoorDistance) {
+    if (other.killable)
+      other.kill();
+    this.open = true;
+    this.sprite = this.world.newEntity(this.openSprite);
+  }
+};
+
+function Hole(w, def) {
+  Trap.call(this, w, def);
+}
+tdl.base.inherit(Hole, Trap);
+
+var minHoleDistance = 0.3;
+Hole.prototype.onCollide = function(other) {
+  if (other.position.sub(this.position).len() < minHoleDistance) {
+    if (other.killable)
+      other.kill();
+  }
+};
