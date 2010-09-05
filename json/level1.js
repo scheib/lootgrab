453abcccb2830581b4c8548af3ecd0bcc171e072
gmurphy <gmurphy@gmail.com>
2010-09-04 20:45:45 -0700

    Update skeleton to use new heading stuff.

--- json/level1.js
+++ json/level1.js
@@ -15,50 +15,17 @@ function getLevelData(level) {
                  "cellWallV", "cellFloorRug",  "cellWallV", "cellFloorRug",  "cellFloorC", "cellFloorC", "cellFloorC", "cellWallV",
                  "cellWallV", "cellFloorC", "cellFloorRug",  "cellFloorRug",  "cellFloorC", "cellFloorC", "cellFloorC", "cellWallV",
                  "cellWallV", "cellFloorC", "cellFloorC", "cellFloorRug",  "cellFloorC", "cellFloorC", "cellFloorC", "cellWallV",
-                 "HeroCell", "SkeletonCell", "SkeletonWarriorCell", "BatCell", "FlamingSkullCell", "MummyCell", "MummyKingCell", "DeathCell"],
+                 "HeroCell", "SkeletonCell", "SkeletonWarriorCell", "BatCell", "FlamingSkullCell", "MummyCell", "cellWallV", "DeathCell"],
         actors : [
           {
             actor_def: "HeroActor",
             position : { x : 2, y : 2 },
             heading : "RIGHT",
           },
-          { actor_def: "SkeletonActor", position : { x : 6, y : 2 } }
+          { actor_def: "SkeletonActor",
+            position : { x : 6, y : 2 },
+            heading : "UP",
+          },
         ],
     }
 }
-
-/*
-var entities = {};
-var cells = {};
-var world;
-
-// Example objects
-function Entity(json) {
-  this.img_url_ = json.img;
-  this.img = new Image();
-  this.img.src = this.img_url_;
-}
-
-function Cell(json) {
-  this.ground = entities[json.group_id];
-  this.entities = [];
-  for (var i = 0, entity_id; entity_id = json.entities[i]; i++) {
-    this.entities.push(entities[entity_id]);
-  }
-}
-
-function World(json) {
-  // etc.
-}
-
-// Use JSON to init our world.
-// Entities must be inited before cells.
-for (var id in w.entities) {
-  entities[id] = new Entity(w.entities[id]);
-}
-// Entities must be inited before world.
-for (var id in w.cells) {
-  cells[id] = new Cell(w.cells[id]);
-}
-world = new World(w.world);
-*/
