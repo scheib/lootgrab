9ce5cc5cb1a847b97158e5cfaf0780fd907b4b57
gman <gman@google.com>
2010-09-04 17:58:48 -0700

    more editor shit

---
+++ lootgrab/render.js
@@ -0,0 +1,66 @@
+tdl.provide('lootgrab.render');
+tdl.require('lootgrab.world');
+
+//////////////////////////////////////////////////
+g_imageDB = {};
+
+function ImageEntity(world, def) {
+  var that = this;
+  var img = g_imageDB[def.img];
+  if(!img) {
+    img = new Image();
+    img.src = def.img;
+    img.onload = function() {
+      tdl.log(that.img.src + " loaded");
+    };
+    img.onerror = function() {
+      tdl.log(that.img.src + " **FAILED**");
+    };
+    g_imageDB[def.img] = img;
+  }
+  this.img = img;
+}
+
+ImageEntity.prototype.draw = function(ctx, x,y,w,h) {
+  ctx.drawImage(this.img, x,y,w,h);
+}
+
+//////////////////////////////////////////////////
+function TileEntity(world, tile_def) {
+  if(tile_def === undefine)
+    throw "Tile def could not be found!"
+
+  var tileset_def = world.getDef(tile_def.tileset)
+  if(tilset_def === undefined)
+    throw "Tileset could not be found"
+}
+TileEntity.prototype.draw = function(ctx, x,y,w,h) {
+  ctx.drawImage(this.img, x,y,w,h);
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+
+function Render(world,ctx) {
+  this.ctx = ctx;
+  this.world = world;;
+}
+
+Render.prototype.draw = function() {
+  var ctx = this.ctx;
+  var w = this.world;
+  ctx.clearRect(0,0,ctx.canvas.width, ctx.canvas.height);
+
+  cell_width = ctx.canvas.width / w.width;
+  cell_height = ctx.canvas.height / w.height;
+
+  for(var i = 0; i < w.cells.length; ++i) {
+    var cell = w.cells[i];
+    x = cell.x * cell_width;
+    y = cell.y * cell_height;
+    w.cells[i].draw(ctx,x,y,cell_width,cell_height);
+  }
+  if(DEBUG)
+    w.draw_dbg(ctx);
+
+}
