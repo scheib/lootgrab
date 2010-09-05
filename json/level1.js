47d017b5e1d6aa7db270b444fa9fb6b49243bfdb
gmurphy <gmurphy@gmail.com>
2010-09-04 20:31:53 -0700

    Add skeletons and collision (death).

---
+++ json/level1.js
@@ -0,0 +1,60 @@
+tdl.provide("level1");
+
+function getLevelData(level) {
+  if (level != "level1")
+    throw "Need to implement more levels!";
+
+    return {
+        name : "world name",
+        width : 8,
+        height : 8,
+        cells : ["wallCell", "wallCell", "wallCell", "wallCell", "wallCell", "wallCell", "wallCell", "wallCell",
+                 "wallCell", "dirtCell", "dirtCell", "rugCell",  "wallCell", "dirtCell", "dirtCell", "wallCell",
+                 "wallCell", "rugCell",  "rugCell",  "rugCell",  "dirtCell", "dirtCell", "dirtCell", "wallCell",
+                 "wallCell", "rugCell",  "dirtCell", "wallCell", "dirtCell", "dirtCell", "dirtCell", "wallCell",
+                 "wallCell", "rugCell",  "wallCell", "rugCell",  "dirtCell", "dirtCell", "dirtCell", "wallCell",
+                 "wallCell", "dirtCell", "rugCell",  "rugCell",  "dirtCell", "dirtCell", "dirtCell", "wallCell",
+                 "wallCell", "dirtCell", "dirtCell", "rugCell",  "dirtCell", "dirtCell", "dirtCell", "wallCell",
+                 "HeroCell", "SkeletonCell", "SkeletonWarriorCell", "BatCell", "FlamingSkullCell", "MummyCell", "wallCell", "DeathCell"],
+        actors : [
+          { actor_def: "HeroActor", position : { x : 2, y : 2 } },
+          { actor_def: "SkeletonActor", position : { x : 6, y : 2 } }
+        ],
+    }
+}
+
+/*
+var entities = {};
+var cells = {};
+var world;
+
+// Example objects
+function Entity(json) {
+  this.img_url_ = json.img;
+  this.img = new Image();
+  this.img.src = this.img_url_;
+}
+
+function Cell(json) {
+  this.ground = entities[json.group_id];
+  this.entities = [];
+  for (var i = 0, entity_id; entity_id = json.entities[i]; i++) {
+    this.entities.push(entities[entity_id]);
+  }
+}
+
+function World(json) {
+  // etc.
+}
+
+// Use JSON to init our world.
+// Entities must be inited before cells.
+for (var id in w.entities) {
+  entities[id] = new Entity(w.entities[id]);
+}
+// Entities must be inited before world.
+for (var id in w.cells) {
+  cells[id] = new Cell(w.cells[id]);
+}
+world = new World(w.world);
+*/
---
+++ lootgrab/actor.js
@@ -0,0 +1,65 @@
+tdl.provide("actor")
+
+tdl.require("lootgrab.math")
+
+/**
+ *
+ * @param {World} w
+ * @param entDef
+ */
+function Actor(w, entDef) {
+  this.world = w;
+  this.entityDef = entDef;
+
+  this.position = ('position' in entDef)
+      ? new Vec2(entDef.position.x + .5,  entDef.position.y + .5)
+      : new Vec2(1.5, 1.5);
+  this.heading = Vec2.RIGHT;
+  this.speed = .025;
+  this.radius = .25;
+
+  this.isAlive = true;
+
+  this.sprite = this.world.newEntity(entDef.sprite);
+
+  this.loot = 'loot' in entDef ? entDef.loot : false;
+  this.passable = 'passable' in entDef ? entDef.passable : true;
+}
+
+Actor.prototype.draw = function(ctx, cw, ch) {
+  // Actor position is center of cell, so subtract 
+  // 0.5 so that we draw it in the right position
+  this.sprite.draw(ctx, 
+      (this.position.x - 0.5) * cw, 
+      (this.position.y - 0.5) * ch,
+      cw, ch);
+}
+
+/**
+ * Update the actor at the beginning of the frame
+ *
+ * @param tick
+ * @param elapsed
+ */
+Actor.prototype.update = function(tick, elapsed) {
+  var nextpos = this.position.add(this.heading.mul(0.5))
+  if (this.world.isBlocking(nextpos.x, nextpos.y)) {
+    this.heading = this.heading.negate();
+  }
+  this.updatePosition();
+}
+
+/**
+ * Inform the actor that it has collided with another actor
+ *
+ * @param {Actor} other
+ */
+Actor.prototype.onCollide = function(other) {
+  
+}
+
+Actor.prototype.updatePosition = function(elapsed) {
+  this.position = this.position.add(
+      this.heading.mul(this.speed));
+}
+
