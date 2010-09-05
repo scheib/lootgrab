3fe3aeaa09358057311b7d9a0861b3efc32bc62e
Ian Lewis <ilewis@google.com>
2010-09-04 21:12:58 -0700

    merge

--- lootgrab/actor.js
+++ lootgrab/actor.js
@@ -8,33 +8,41 @@ tdl.require("lootgrab.math")
  * @param entDef
  */
 function Actor(w, entDef) {
+  try {
   if (entDef === undefined)
     console.error("Undefined entity def.");
   this.world = w;
   this.entityDef = entDef;
 
-  this.position = ('position' in entDef)
-      ? new Vec2(entDef.position.x + .5,  entDef.position.y + .5)
-      : new Vec2(1.5,1.5);
-  this.heading = Vec2.CENTER;
-  this.speed = .025;
-  this.radius = .25;
+    this.position = ('position' in entDef)
+        ? new Vec2(entDef.position.x + .5,  entDef.position.y + .5)
+        : new Vec2(1.5,1.5);
+    this.heading = Vec2.CENTER;
+    this.speed = .025;
+    this.radius = .25;
 
-  this.isAlive = true;
+    this.isAlive = true;
 
-  this.sprite = this.world.newEntity(entDef.sprite);
+    this.sprite = this.world.newEntity(entDef.sprite);
 
-  this.loot = 'loot' in entDef ? entDef.loot : false;
-  this.passable = 'passable' in entDef ? entDef.passable : true;
+    this.loot = 'loot' in entDef ? entDef.loot : false;
+    this.passable = 'passable' in entDef ? entDef.passable : true;
+  } catch (err) {
+    alert("Couldn't create Actor: " + err.toString());
+  }
 }
 
 Actor.prototype.init = function(instanceDef) {
-  if ('position' in instanceDef) {
-    var pos = new Vec2(instanceDef.position.x + .5, instanceDef.position.y + .5);
-    this.position = pos;
-  }
-  if ('heading' in instanceDef) {
-    this.heading = eval("Vec2." + instanceDef.heading)
+  try {
+    if ('position' in instanceDef) {
+      var pos = new Vec2(instanceDef.position.x + .5, instanceDef.position.y + .5);
+      this.position = pos;
+    }
+    if ('heading' in instanceDef) {
+      this.heading = eval("Vec2." + instanceDef.heading)
+    }
+  } catch (err) {
+    alert("Couldn't initialize Actor: " + err.toString());
   }
 }
 
