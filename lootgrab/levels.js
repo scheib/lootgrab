92d836ed312547178279d5ee3cff1b651efd4530
Vincent Scheib <scheib@gmail.com>
2010-09-07 16:03:40 -0700

    Levels sorted in UI, Tutorials first. A Tutorial added. Default level is tut1.

--- lootgrab/levels.js
+++ lootgrab/levels.js
@@ -4,13 +4,27 @@ tdl.require('tdl.io');
 
 lootgrab.levels = (function() {
 
+  // UI will sort these by the name defined in the level - not the filename.
   var urls_ = [
-    "../json/tutorial-key-sword.level",
+    //default: "../json/tutorial-1-key-sword.level",
+    "../json/tutorial-2-loot.level",
+
+    "../json/gamejam.level",
     "../json/enne-fire_alley.level",
   ];
 
   var levels_ = [];
 
+  function levelSortFunc(a,b) {
+    a = a.name.toLowerCase();
+    b = b.name.toLowerCase();
+    aIsTut = a.indexOf("tutorial") == 0;
+    bIsTut = b.indexOf("tutorial") == 0;
+    if (aIsTut != bIsTut)
+      return aIsTut < bIsTut;
+    return a > b;
+  }
+        
   function loadLevel(url) {
     url = tdl.io.getAbsoluteURI(url);
     tdl.io.loadJSON(url, function(json, exception) {
@@ -19,6 +33,7 @@ lootgrab.levels = (function() {
       } else {
         tdl.log("Loaded: ", url);
         levels_.push(json);
+        levels_.sort(levelSortFunc);
       }
     });
   }
@@ -31,6 +46,7 @@ lootgrab.levels = (function() {
       }
     }
     levels_.push(level);
+    levels_.sort(levelSortFunc);
   }
 
   function getLevels() {
